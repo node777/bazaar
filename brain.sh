2a7aee321ab5a6b5af6140780b3ddc672248de20
traplizard <34205710+traplizard@users.noreply.github.com>
2020-04-03 04:23:28 -0700

    Add files via upload

--- brain.sh
+++ brain.sh
@@ -93,7 +93,7 @@ then
     
         for i in ${missing[@]}
         do
-            echo "${missing[*]}"
+            echo "${missing[$i]}"
         done
 
     read -r -p "would you like to install now? [y/N] " response
@@ -126,15 +126,15 @@ then
                     #apt-get install docker-ce docker-ce-cli containerd.io
 
                     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-                    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-                    sudo apt update
-                    sudo apt-get install docker-ce
+                    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+                    apt update
+                    apt-get install docker-ce
 
-                    sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-                    sudo chmod +x /usr/local/bin/docker-compose
+                    curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+                    chmod +x /usr/local/bin/docker-compose
 
                     # add yourself to the docker group and re-login
-                    sudo usermod -aG docker ${USER}
+                    usermod -aG docker ${USER}
                 ;;
                 go)
                     #wget https://dl.google.com/go/go1.14.1.linux-amd64.tar.gz
